470930f6918ebaabc1746ee95cc109647c5ae783
Persona4dan <desu2dan@gmail.com>
2025-02-03 10:41:00 -0600

    chore: package installs

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App.jsx";
-import { Provider } from "react-redux";
 import store from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
