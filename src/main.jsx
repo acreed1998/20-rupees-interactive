5752c791fa7c7b7e65c0e2e21c25c0cb557cf98a
Persona4dan <desu2dan@gmail.com>
2025-02-05 15:28:45 -0600

    feat: add theme and theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,16 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
 import store from "./redux/store.js";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </StrictMode>
 );
