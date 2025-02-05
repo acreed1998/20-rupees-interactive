a8e1c58ff91867ca54a8ab64d142cd0da3f2fa4b
Persona4dan <desu2dan@gmail.com>
2025-02-05 15:44:24 -0600

    feat: add logic for dialog component

--- src/components/dialog/index.jsx
+++ src/components/dialog/index.jsx
@@ -1,14 +1,37 @@
-import { DialogContent, DialogTitle, Dialog as MUIDialog } from "@mui/material";
-import { useSelector } from "react-redux";
+import {
+  DialogContent,
+  DialogTitle,
+  Grid2,
+  IconButton,
+  Dialog as MUIDialog,
+  useMediaQuery,
+} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
 import { getDialogOpen } from "../../redux/selectors";
+import { toggleDialogOpen } from "../../redux/slices/mainSlice";
+import { Close } from "@mui/icons-material";
 
 export const Dialog = () => {
+  const dispatch = useDispatch();
   const dialogOpen = useSelector(getDialogOpen);
+  const isSVP = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
+  const onClose = () => dispatch(toggleDialogOpen());
 
   return (
-    <MUIDialog aria-labelledby="20-rupees-dialog-title" open={dialogOpen}>
-      <DialogTitle id="20-rupees-dialog-title">Final Build</DialogTitle>
-      <DialogContent></DialogContent>
+    <MUIDialog
+      aria-labelledby="20-rupees-dialog-title"
+      open={dialogOpen}
+      onClose={onClose}
+      fullScreen={isSVP}
+    >
+      <Grid2 className="bg-red-500" container justifyContent="space-between">
+        <DialogTitle id="20-rupees-dialog-title">Final Build</DialogTitle>
+        <IconButton className="mr-4" onClick={onClose}>
+          <Close />
+        </IconButton>
+      </Grid2>
+      <DialogContent>something</DialogContent>
     </MUIDialog>
   );
 };
